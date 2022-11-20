cc7824c36efd839a6d1ced8b5fd9c6a7c11c6a4f
Bavalionprolate <codethegit@gmail.com>
2022-11-20 20:47:28 +0800

    Upload application

---
+++ 19261_face_look.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: db
+-- Время создания: Ноя 18 2022 г., 06:53
+-- Версия сервера: 10.9.3-MariaDB-1:10.9.3+maria~ubu2204
+-- Версия PHP: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `19261_face_look`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Photo_user`
+--
+
+CREATE TABLE `Photo_user` (
+  `User_id` int(11) NOT NULL,
+  `photo` longblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Prava`
+--
+
+CREATE TABLE `Prava` (
+  `User_id` int(11) NOT NULL,
+  `is_admin` bit(1) DEFAULT NULL,
+  `is_programm_user` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `User`
+--
+
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL,
+  `login` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `fio` varchar(100) DEFAULT NULL,
+  `pol` varchar(45) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `address` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `Photo_user`
+--
+ALTER TABLE `Photo_user`
+  ADD KEY `fk_Photo_user_User1_idx` (`User_id`);
+
+--
+-- Индексы таблицы `Prava`
+--
+ALTER TABLE `Prava`
+  ADD KEY `fk_Prava_User_idx` (`User_id`);
+
+--
+-- Индексы таблицы `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `Photo_user`
+--
+ALTER TABLE `Photo_user`
+  ADD CONSTRAINT `fk_Photo_user_User1` FOREIGN KEY (`User_id`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `Prava`
+--
+ALTER TABLE `Prava`
+  ADD CONSTRAINT `fk_Prava_User` FOREIGN KEY (`User_id`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ app.py
@@ -0,0 +1,68 @@
+import sys
+import pymysql as mdb
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QMessageBox
+
+
+from assets.ui.app_ui import Ui_MainWindow
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.appUI = Ui_MainWindow()
+        self.appUI.setupUi(self)
+        self.appUI.stackedWidget.setCurrentIndex(0)
+        self.appUI.btn_home_2.setChecked(True)
+
+        self.appUI.middle_sidebar_widget.hide()
+
+
+    ## Change QPushButton Checkable status when stackedWidget index changed
+    def on_stackedWidget_currentChanged(self, index):
+        btn_list = self.appUI.middle_sidebar_widget.findChildren(QPushButton) \
+                    + self.appUI.full_sidebar_widget.findChildren(QPushButton)
+        
+        for btn in btn_list:
+            if index in [4, 4]:
+                btn.setAutoExclusive(False)
+                btn.setChecked(False)
+            else:
+                btn.setAutoExclusive(True)
+            
+    ## functions for changing menu page
+    def on_btn_home_1_toggled(self):
+        self.appUI.stackedWidget.setCurrentIndex(0)
+    
+    def on_btn_home_2_toggled(self):
+        self.appUI.stackedWidget.setCurrentIndex(0)
+
+    def on_btn_db_1_toggled(self):
+        self.appUI.stackedWidget.setCurrentIndex(1)
+
+    def on_btn_db_2_toggled(self):
+        self.appUI.stackedWidget.setCurrentIndex(1)
+
+    def on_btn_add_1_toggled(self):
+        self.appUI.stackedWidget.setCurrentIndex(2)
+
+    def on_btn_add_2_toggled(self):
+        self.appUI.stackedWidget.setCurrentIndex(2)
+
+    def on_btn_settings_1_toggled(self):
+        self.appUI.stackedWidget.setCurrentIndex(3)
+
+    def on_btn_settings_2_toggled(self):
+        self.appUI.stackedWidget.setCurrentIndex(3)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    with open("assets/style/app_style.qss", "r") as style_file:
+        style_str = style_file.read()
+
+    app.setStyleSheet(style_str)
+
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
