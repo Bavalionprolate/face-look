6f3a4d80aaeb68f24883b64529f2b9543827cfed
Bavalionprolate <codethegit@gmail.com>
2022-11-29 21:18:34 +0800

    upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys, mysql.connector #mysql-connector-python==8.0.29
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem, QPushButton, QMessageBox, QHeaderView, QAbstractItemView
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem, QPushButton, QMessageBox, QHeaderView, QAbstractItemView, QDialog, QFileDialog
 from PyQt5.QtCore import pyqtSlot, QObject, QEvent, Qt
 from PyQt5.QtGui import QPixmap
 from datetime import datetime
@@ -56,16 +56,23 @@ class ConnectToMySQL():
 			if self.con:
 				self.con.close()
 
-	def UploadData(self, lineEdit_id, lineEdit_login, lineEdit_password, lineEdit_fio, lineEdit_pol, lineEdit_data, lineEdit_addres):
+	def UploadData(self, lineEdit_id, lineEdit_login, lineEdit_password, lineEdit_fio, lineEdit_pol, lineEdit_data, lineEdit_addres, label_status):
 		try:
 			self.connect()
-			cursor = self.con.cursor(dictionary=True)
-			sql = f"INSERT INTO User (`id`, `login`, `password`, `fio`, `pol`, `birthday`, `address`) VALUES ('{lineEdit_id}', '{lineEdit_login}', '{lineEdit_password}', '{lineEdit_fio}', '{lineEdit_pol}', '{lineEdit_data}', '{lineEdit_addres}')"
+			cursor = self.con.cursor()
+			sql = f"INSERT INTO User (`id`, `login`, `password`, `fio`, `pol`, `birthday`, `address`) VALUES ('{lineEdit_id}', '{lineEdit_login}', '{lineEdit_password}', '{lineEdit_fio}', '{lineEdit_pol}', '{lineEdit_data}', '{lineEdit_addres}');"
 			cursor.execute(sql)
-			result = cursor.fetchone()
 			self.con.commit()
+			
 
-			return result
+			with open(label_status, 'rb') as f:
+				photo = f.read()
+			encodestring = base64.b64encode(photo)
+			
+			sql = f"INSERT INTO photo_user (User_id, photo) values ({lineEdit_id}, %s)"
+			cursor.execute(sql, (encodestring, ))
+			self.con.commit()
+				
 		except Exception as ex:
 			msgBox = QMessageBox()
 			msgBox.setIcon(QMessageBox.Information)
@@ -74,6 +81,7 @@ class ConnectToMySQL():
 			msgBox.setStandardButtons(QMessageBox.Ok)
 			msgBox.exec()
 
+			self.con.rollback() 
 
 		finally:
 			msgBox = QMessageBox()
@@ -84,6 +92,7 @@ class ConnectToMySQL():
 			msgBox.exec()
 			if self.con:
 				self.con.close()
+
 class MainWindow(QtWidgets.QMainWindow):
 	def __init__(self):
 		super(MainWindow, self).__init__() 
@@ -96,42 +105,50 @@ class MainWindow(QtWidgets.QMainWindow):
 
 		self.appUI.middle_sidebar_widget.hide()
 
-		self.btn_get_date = self.appUI.btn_get_date
+		self.btn_get_date_1 = self.appUI.btn_get_date_1
+		self.btn_get_date_2 = self.appUI.btn_get_date_2
 		self.btn_add_user_in_bd = self.appUI.btn_add_user_in_bd
+		self.btn_upload_user_photo = self.appUI.btn_upload_user_photo
+		self.btn_clear_table_1 = self.appUI.btn_clear_table_1
+		self.btn_clear_table_2 = self.appUI.btn_clear_table_2
 		
-		self.result_table = self.appUI.tableWidget
+		self.result_table_1 = self.appUI.tableWidget
+		self.result_table_2 = self.appUI.tableWidget_2
 
-		self.result_table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
-		self.result_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-		self.result_table.horizontalHeader().setMinimumSectionSize(0)
-		self.result_table.viewport().installEventFilter(self)
+		self.result_table_1.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+		self.result_table_1.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+		self.result_table_1.horizontalHeader().setMinimumSectionSize(0)
+		self.result_table_1.viewport().installEventFilter(self)
 
-		self.btn_get_date.clicked.connect(self.on_btn_get_date)
+		self.btn_get_date_1.clicked.connect(self.on_btn_get_date_1)
+		self.btn_get_date_2.clicked.connect(self.on_btn_get_date_2)
 		self.btn_add_user_in_bd.clicked.connect(self.on_btn_add_user_in_bd)
+		self.btn_upload_user_photo.clicked.connect(self.on_btn_upload_user_photo)
+		self.btn_clear_table_1.clicked.connect(self.on_btn_clear_table_1)
+		self.btn_clear_table_2.clicked.connect(self.on_btn_clear_table_2)
 
 	def getsamerowcell(self, columnname):
 
-		row = self.result_table.currentItem().row()
+		row = self.result_table_1.currentItem().row()
 		# col = widget.currentItem().column()
 
 		# loop through headers and find column number for given column name
-		headercount = self.result_table.columnCount()
+		headercount = self.result_table_1.columnCount()
 		for x in range(headercount):
-			headertext = self.result_table.horizontalHeaderItem(x).text()
+			headertext = self.result_table_1.horizontalHeaderItem(x).text()
 			if columnname == headertext:
-				cell = self.result_table.item(row, x).text()  # get cell at row, col
+				cell = self.result_table_1.item(row, x).text()  # get cell at row, col
 				return cell
 		
-		
 	def eventFilter(self, source, event):
-		if self.result_table.selectedIndexes() != []:
+		if self.result_table_1.selectedIndexes() != []:
 			
 			if event.type() == QEvent.MouseButtonRelease:
 				if event.button() == Qt.LeftButton:
 					
-					col = self.result_table.currentColumn()
+					col = self.result_table_1.currentColumn()
 
-					if (col == 7):
+					if (col == 6):
 						try:
 							db = ConnectToMySQL()
 							image = db.RetriveBlob(self.getsamerowcell('Идентификатор'))
@@ -153,6 +170,21 @@ class MainWindow(QtWidgets.QMainWindow):
 		return QObject.event(source, event)
 
 	@pyqtSlot(bool)
+	def on_btn_clear_table_1(self):
+		while (self.result_table_1.rowCount() > 0):
+			self.result_table_1.removeRow(0)
+
+	@pyqtSlot(bool)
+	def on_btn_clear_table_2(self):
+		while (self.result_table_2.rowCount() > 0):
+			self.result_table_2.removeRow(0)
+	
+	@pyqtSlot(bool)
+	def on_btn_upload_user_photo(self):
+		fname = QFileDialog.getOpenFileName(self, 'Open file', "")
+		self.appUI.label_status.setText(fname[0])
+
+	@pyqtSlot(bool)
 	def on_btn_add_user_in_bd(self):
 		lineEdit_login = self.appUI.lineEdit_login.text()
 		lineEdit_id = self.appUI.lineEdit_id.text()
@@ -161,17 +193,55 @@ class MainWindow(QtWidgets.QMainWindow):
 		lineEdit_pol = self.appUI.lineEdit_pol.text()
 		lineEdit_data = self.appUI.lineEdit_data.text()
 		lineEdit_addres = self.appUI.lineEdit_addres.text()
-		db = ConnectToMySQL()
-		db.UploadData(lineEdit_id, lineEdit_login, lineEdit_password, lineEdit_fio, lineEdit_pol, lineEdit_data, lineEdit_addres)
-		
-
+		label_status = self.appUI.label_status.text()
+		if (label_status == ''):
+			msgBox = QMessageBox()
+			msgBox.setIcon(QMessageBox.Information)
+			msgBox.setText("Вы не добавили фото!\nДобавьте обязательно фотографию")
+			msgBox.setWindowTitle("Ошибка")
+			msgBox.setStandardButtons(QMessageBox.Ok)
+			msgBox.exec()
+		elif (lineEdit_id == "" or lineEdit_password == "" or lineEdit_fio == "" or lineEdit_pol  == "" or lineEdit_data == "" or lineEdit_addres == ""):
+			msgBox = QMessageBox()
+			msgBox.setIcon(QMessageBox.Information)
+			msgBox.setText("Вы не заполнили все строки данными")
+			msgBox.setWindowTitle("Ошибка")
+			msgBox.setStandardButtons(QMessageBox.Ok)
+			msgBox.exec()
+		else:
+			db = ConnectToMySQL()
+			db.UploadData(lineEdit_id, lineEdit_login, lineEdit_password, lineEdit_fio, lineEdit_pol, lineEdit_data, lineEdit_addres, label_status)
 
 	@pyqtSlot(bool)
-	def on_btn_get_date(self):
+	def on_btn_get_date_1(self):
 		result = ConnectToMySQL().get_all_data_from_db()
 
 		if result:
-			self.result_table.setRowCount(len(result))
+			self.result_table_1.setRowCount(len(result))
+
+			for row, item in enumerate(result):
+				column_1_item = QtWidgets.QTableWidgetItem(str(item['id']))
+				column_2_item = QtWidgets.QTableWidgetItem(str(item['login']))
+				column_3_item = QtWidgets.QTableWidgetItem(str(item['fio']))
+				column_4_item = QtWidgets.QTableWidgetItem(str(item['pol']))
+				column_5_item = QtWidgets.QTableWidgetItem(str(item['birthday']))
+				column_6_item = QtWidgets.QTableWidgetItem(str(item['address']))
+				column_7_item = QtWidgets.QTableWidgetItem(str('view'))			
+
+				self.result_table_1.setItem(row, 0, column_1_item)
+				self.result_table_1.setItem(row, 1, column_2_item)
+				self.result_table_1.setItem(row, 2, column_3_item)
+				self.result_table_1.setItem(row, 3, column_4_item)
+				self.result_table_1.setItem(row, 4, column_5_item)
+				self.result_table_1.setItem(row, 5, column_6_item)
+				self.result_table_1.setItem(row, 6, column_7_item)
+
+	@pyqtSlot(bool)
+	def on_btn_get_date_2(self):
+		result = ConnectToMySQL().get_all_data_from_db()
+
+		if result:
+			self.result_table_2.setRowCount(len(result))
 
 			for row, item in enumerate(result):
 				column_1_item = QtWidgets.QTableWidgetItem(str(item['id']))
@@ -180,23 +250,19 @@ class MainWindow(QtWidgets.QMainWindow):
 				column_4_item = QtWidgets.QTableWidgetItem(str(item['fio']))
 				column_5_item = QtWidgets.QTableWidgetItem(str(item['pol']))
 				column_6_item = QtWidgets.QTableWidgetItem(str(item['birthday']))
-				column_7_item = QtWidgets.QTableWidgetItem(str(item['address']))
-				column_8_item = QtWidgets.QTableWidgetItem(str('view'))			
+				column_7_item = QtWidgets.QTableWidgetItem(str(item['address']))	
 
-				self.result_table.setItem(row, 0, column_1_item)
-				self.result_table.setItem(row, 1, column_2_item)
-				self.result_table.setItem(row, 2, column_3_item)
-				self.result_table.setItem(row, 3, column_4_item)
-				self.result_table.setItem(row, 4, column_5_item)
-				self.result_table.setItem(row, 5, column_6_item)
-				self.result_table.setItem(row, 6, column_7_item)
-				self.result_table.setItem(row, 7, column_8_item)
+				self.result_table_2.setItem(row, 0, column_1_item)
+				self.result_table_2.setItem(row, 1, column_2_item)
+				self.result_table_2.setItem(row, 2, column_3_item)
+				self.result_table_2.setItem(row, 3, column_4_item)
+				self.result_table_2.setItem(row, 4, column_5_item)
+				self.result_table_2.setItem(row, 5, column_6_item)
+				self.result_table_2.setItem(row, 6, column_7_item)
 
 		else:
 			QMessageBox.information(self, 'Warning', 'No date got from database')
 			return
-		
-
 
 	## Change QPushButton Checkable status when stackedWidget index changed
 	def on_stackedWidget_currentChanged(self, index):
